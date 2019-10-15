5c7e8d4f953f67045c9f4884e7518ccdef723fa2
satoshi shimazaki <satoshi@brightning.co.jp>
2019-10-15 15:38:42 +0900

    bashで動かすほうを追加

---
+++ wsl/open-x410-linux-tilix.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM ### Start X410 in Windowed Apps Mode. If X410 is already running in Desktop Mode, 
+REM ### it'll be terminated first without any warning message box.
+
+start /B x410.exe /wm
+
+REM ### Setup a D-Bus instance that will be shared by all X-Window apps
+
+pengwin.exe run "sh -ic 'if [ -z \"(pidof dbus-launch)\" ]; then export DISPLAY=127.0.0.1:0.0; dbus-launch --exit-with-x11; fi;'"
+
+REM ### Go to the selected folder path and open your terminal app
+
+pengwin.exe run "cd  \"$(wslpath '%1')\"; exec tilix --display=:0.0 &"
